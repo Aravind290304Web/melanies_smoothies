f2834434079d1dda8d65b68b4e2be90541156dbb
Aravind290304Web <aravind2prdp@gmail.com>
2026-09-08 01:24:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import os
+import pandas as pd
+import requests 
 
 # Write directly to the app
 st.title(f"Custom Smoothie Order Form :cup_with_straw: ")
@@ -15,9 +17,16 @@ st.write('The name on your smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+pd_df = my_dataframe.to_pandas()
+#st.dataframe(pd_df)
+#st.stop()
+
 #st.dataframe(data=my_dataframe, use_container_width=True)
-ingredients_list = st.multiselect('Choose up to 5 ingredients', my_dataframe)
+ingredients_list = st.multiselect('Choose up to 5 ingredients', my_dataframe, max_selections=5)
 
 if ingredients_list:
     
@@ -40,8 +49,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
-#New section to display smoothiefruit nutrition information
-import requests  
+#New section to display smoothiefruit nutrition information 
 
 
 if ingredients_list:
@@ -49,6 +57,10 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+      
         st.subheader(fruit_chosen + 'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen)
+        smoothiefroot_response = requests.get("https://www.smoothiefroot.com/api/fruit/{search_on}")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
